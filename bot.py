c8aebdb12c247e9839e064ecc93c69411b747712
ethernetcable <ethernetcable@users.noreply.github.com>
2016-11-14 14:18:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,42 @@ import discord
 from bs4 import BeautifulSoup
 from urllib import request
 from time import sleep 
+from random import randint
 
 client = discord.Client()
 token = 'xxxx'
+
+user = ''
+
+shank_bank = ['\*shanks %s*',
+              '\*shivs %s*',
+              '\*rips out %s\'s heart and feeds it to him*',
+              '\*stabs %s*',
+              '\*pushes %s off a bridge*',
+              '\*feeds on %s\'s brain*',
+              '\*kills %s to death*',
+              '\*%s does a backflip off trump tower*',
+              '\*%s reads a wikihow article on how to die*',
+              '\*%s dies of old age*',
+              '\*%s falls over and dies*',
+              '\*%s hooks up with a nigerian prince, proceeds '
+              'to die of bankruptcy']
+
+ong_words = ['strong',
+             'pong',
+             'glong',
+             'fong',
+             'kong',
+             'long',
+             'trong',
+             'frong',
+             'tong',
+             'extremely long',
+             'donald trong',
+             'michael gong',
+             'kit smong',
+             'jomes phillong',
+             'aaron hong']
 
 async def wikihow(message):
     try:
@@ -18,7 +51,8 @@ async def wikihow(message):
 
 async def bigben(message):
     try:
-        msg = 'bong this bong that just shut your godddamn cakehole'
+        num = randint(0, len(ong_words) - 1)
+        msg = ong_words[num]
         await client.send_message(message.channel, msg)
         print('sent: ' + msg)
     except:
@@ -31,13 +65,25 @@ async def test(message):
     except:
         print('test fail')
 
+async def kill(message):
+    try:
+        user = message.content.split()[1]
+        num = randint(0, len(shank_bank) - 1)
+        msg = shank_bank[num]
+        await client.send_message(message.channel, msg % user)
+        print('killed someone')
+    except:
+        print('kill failed')
+
 @client.event
 async def on_message(message):
     if message.content.startswith('!wikihow'):
         await wikihow(message)
-    if message.content.startswith('BONG'):
+    if 'BONG' in message.content:
         await bigben(message)
     if message.content.startswith('test'):
         await test(message)
+    if message.content.startswith('!kill'):
+        await kill(message)
 
 client.run(token)
