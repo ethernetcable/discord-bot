7dc5cf6a964767dd02ff5d218bef6f441347c3a1
louis <ethernetcable@users.noreply.github.com>
2016-12-29 23:09:03 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,20 +12,6 @@ CONSUMER_KEY = 'yyyy'
 CONSUMER_SECRET = 'zzzz'
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET)
 
-shank_bank = ['\*shanks %s*',
-              '\*shivs %s*',
-              '\*rips out %s\'s heart and feeds it to him*',
-              '\*stabs %s*',
-              '\*pushes %s off a bridge*',
-              '\*feeds on %s\'s brain*',
-              '\*kills %s to death*',
-              '\*%s does a backflip off trump tower*',
-              '\*%s reads a wikihow article on how to die*',
-              '\*%s dies of old age*',
-              '\*%s falls over and dies*',
-              '\*%s hooks up with a nigerian prince, proceeds '
-              'to die of bankruptcy*']
-
 async def wikihow(message):
     try:
         url = 'http://www.wikihow.com/Special:Random'
@@ -44,16 +30,6 @@ async def test(message):
         print('test success')
     except:
         print('test fail')
-
-async def kill(message):
-    try:
-        user = message.content.split()[1]
-        num = randint(0, len(shank_bank) - 1)
-        msg = shank_bank[num]
-        await client.send_message(message.channel, msg % user)
-        print('killed someone')
-    except:
-        print('kill failed')
 
 async def commands(message):
     try:
@@ -88,15 +64,6 @@ async def trump(message):
         sleep(0.1)
         print('retrying trump')
         await trump(message)
-
-async def kit(message):
-    try:
-        await client.send_message(message.channel, 'kit smells')
-        print('kit smells')
-    except:
-        sleep(0.1)
-        print('retrying kit')
-        await kit(message)
         
 @client.event
 async def on_ready():
@@ -109,18 +76,14 @@ async def on_message(message):
         await wikihow(message)
     if message.content.startswith('!test'):
         await test(message)
-    if message.content.startswith('!kill'):
-        await kill(message)
     if message.content.startswith('!wikicommands'):
         await commands(message)
     if message.content.startswith('!ouch'):
         await ouch(message)
     uppers = [i for i in message.content if i.isupper()]
-    if len(uppers) > 7 and author.user.id != '159985870458322944':
+    if len(uppers) > 7:
         await shout(message)
     if message.content.startswith('!trump'):
         await trump(message)
-    if message.content.startswith('!rank')
-        await kit(message)
 
 client.run(token)
